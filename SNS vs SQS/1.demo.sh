292888af2e58e71ea6faedb1cce391563b607dd3
Bharathkumar Dasaraju <bharathkumar.dasaraju@singtel.com>
2021-11-26 17:29:17 +0800

    :stethoscope: difference between SNS and SQS :stethoscope:

--- SNS vs SQS/1.demo.sh
+++ SNS vs SQS/1.demo.sh
@@ -1,17 +1,48 @@
 SNS:
---------------------------------------------------------------------------------->
+------------------------------------------------------------------------------------------------------------------------->
 
 SNS uses a publisher subscriber system
 
 So we may own a Topic and we publish to that topic. 
 And subscribers get notified of events that are delivered to that topic.
 
+publishing a message to a topic can deliver-to too many subscribers.
 
+Those suscribers can be of different types(SQS, Lambda, Email).
+So you can have SQS a subscriber or a lambda function or even an Email as a subscriber.
+
+
+Question:
+----------------->
+Do other systems care about an event? YES
 
 SQS:
---------------------------------------------------------------------------------->
+-------------------------------------------------------------------------------------------------------------------------->
 
 Its a queueing service for message processing
+
+so this example on SQS could be a subscriber to an SNS.
+so whenever someone publishes message to an SNS your SQS queue can get a message in it that can be processed at a later time.
+
+Actually a system that must poll the queue to discover new events.
+
+So whenever an event is delivered to the queue there is nothing thats gonna get invoked. there is no system that automatically can become aware of it.
+you need to have some separate thread thats polling the queue to discover when new events get delivered and have a mechanism to actually process it and then delete the message from the queue.
+
+So messages in the queue are typically processed by a single consumer or a single service with a very narrow responsibility.
+
+And different services if they both care about the same events they can have their own separate queues. 
+
+
+Question:
+----------------->
+Does your system care about an event? YES  --> do you care about the data cause you are the one recieve the data.
+
+
+
+
+
+
 
 
 
---
+++ SNS vs SQS/2.demo_example.sh
@@ -0,0 +1,44 @@
+example:
+-------------------->
+
+1. An user making a purchase may be on some website or through some POS terminal and putting in their credentials to by some kind of product.
+
+2. Now they are making a purchase request to some kind of REST API and we are just calling this Transaction Processing Webservice.
+
+3. And the payload of what gets passed into this webservice looks like below.
+
+   {
+       "TransactionId": "123456",
+       "CustomerId": "CID_123",
+       "CustomerEmail": "bkr@gmail.com",
+       "Amount": 5000.000
+   }
+
+4. From the processing perspective if we get a request containing this kind of event, the first thing we need to do is communicate with some kind of creditcard authority service.
+   Practically speaking probably Visa,Mastercard,Amex all these creditcard authority services/
+
+5. So when an event occurs saying that someone is attempting to purchase somethig we need to validate that against the authority service.
+
+6. So the first thing we do is, you probably call that service and say here is the CustomerID , may be they passed in some credentials including their PIN or something like that.
+   and that get validates aganinst the authority service, and in this example lets just assume that they returned 200 Okay, alright the Transaction is good, the credentials are correct this thing went through.
+
+7. Now in this ecommerce example this is an event that matters right, because someone attempted to purchase something and it went through so it was successful and this is something that is potentially 
+   relevant to many different subsystems that exists in your ecommerce ecosystem so may be we want to publish this event, i.e. this Transaction to SNS Topic right.
+
+8. So you are basically telling the world that you are pulishing an event to this SNS Topic, and from this topic there can be multiple different subscribers with all narrow usecases.
+   So in this example we have three different ones i.e. one lambda functiona and two SQS queues(shown in the picture - )
+
+9. In the first case when a message gets delivered may be we have some kind of customer reminder service(A lambda function) - this lambda fucntion delvers message(via Email) to customer saying that Thank you for ordering we appreciate your business
+
+10. And completely separately there is two other subsystems(two SQS Queue's)' here they care about this same event but they do very different things so we dont want to mix the responsibilities of these different things.
+
+11. We want to have a separate systems that allows us to process these events in parallel and completely independent of one another.
+
+12. May be 
+
+
+
+
+
+
+
