2408b6a4da242a7823c7b17a1ac26be090559315
Bharathkumar Dasaraju <bharathkumar.dasaraju@singtel.com>
2021-11-26 14:16:14 +0800

    :boom: in SNS you own a topic and you publish to that topic, subscribers get notified of events that are delivered to that topic :boom:

---
+++ SNS vs SQS/1.demo.sh
@@ -0,0 +1,17 @@
+SNS:
+--------------------------------------------------------------------------------->
+
+SNS uses a publisher subscriber system
+
+So we may own a Topic and we publish to that topic. 
+And subscribers get notified of events that are delivered to that topic.
+
+
+
+SQS:
+--------------------------------------------------------------------------------->
+
+Its a queueing service for message processing
+
+
+
